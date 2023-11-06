03adc707e3a6423b134725774e111704a5981bbf
Max Gasaro <massimilianogasaro@me.com>
2023-11-06 16:05:40 +0100

    fix env variables

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,42 +2,43 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject, tap } from 'rxjs';
 import { Post } from '../models/post';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
  providedIn: 'root'
 })
 
 export class PostService {
- private url = 'http://localhost:5200';
- private posts$: Subject<Post[]> = new Subject();
+  private url: string = environment.apiUrl;
+  private posts$: Subject<Post[]> = new Subject();
 
- constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
- private refreshPosts() {
+  private refreshPosts() {
     this.httpClient.get<Post[]>(`${this.url}/posts`)
       .subscribe(posts => {
         this.posts$.next(posts);
       });
- }
+  }
 
- getPosts(): Subject<Post[]> {
+  getPosts(): Subject<Post[]> {
     this.refreshPosts();
     return this.posts$;
- }
+  }
 
- getPost(id: string): Observable<Post> {
+  getPost(id: string): Observable<Post> {
     return this.httpClient.get<Post>(`${this.url}/posts/${id}`);
- }
+  }
 
- createPost(post: Post): Observable<string> {
+  createPost(post: Post): Observable<string> {
     return this.httpClient.post(`${this.url}/posts`, post, { responseType: 'text' });
- }
+  }
 
- updatePost(id: string, post: Post): Observable<string> {
+  updatePost(id: string, post: Post): Observable<string> {
     return this.httpClient.put(`${this.url}/posts/${id}`, post, { responseType: 'text' });
- }
+  }
 
- deletePost(id: string): Observable<string> {
+  deletePost(id: string): Observable<string> {
     return this.httpClient.delete(`${this.url}/posts/${id}`, { responseType: 'text' });
- }
+  }
 }
