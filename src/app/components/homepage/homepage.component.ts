ebed60a2b097bd810d227c008b3fa5a5fc484583
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2023-11-07 17:16:25 +0100

    init responsive design

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver,Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  constructor(private responsive: BreakpointObserver) {}
 
+  isXSmall: boolean = false;
+  isSmall: boolean = false;
+  isMedium: boolean = false;
+
+  ngOnInit(): void {
+    this.responsive.observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium])
+      .subscribe(result => {
+        const breakpoints = result.breakpoints;
+        if (breakpoints[Breakpoints.XSmall]) {
+          this.isXSmall = true;
+          this.isSmall = false;
+          this.isMedium = false;
+        }
+        else if (breakpoints[Breakpoints.Small]) {
+          this.isXSmall = false;
+          this.isSmall = true;
+          this.isMedium = false;
+        }
+        else if (breakpoints[Breakpoints.Medium]) {
+          this.isXSmall = false;
+          this.isSmall = false;
+          this.isMedium = true;
+        }
+    });
+  }
 }
